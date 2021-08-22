2df693b5d3015f48d44e2b9d82aebbc915a90059
leandrop <leandro.p.n96@gmail.com>
2021-08-21 21:22:57 -0300

    feat: :sparkles: Implementado o metodo post para cadastro de usuario

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,7 +1,21 @@
 module.exports.cadastro = function (application, req, res) {
-    res.render('cadastro');
+    res.render('cadastro', { validacao: {}, dadosForm: {} });
 }
 
-module.exports.catastrar = function (application, req, res) {
+module.exports.cadastrar = function (application, req, res) {
+
+    var data = req.body
+
+    req.assert('nome', 'Nome não pode ser zazio').notEmpty()
+    req.assert('usuario', 'Usuário não pode ser zazio').notEmpty()
+    req.assert('senha', 'Sennha não pode ser zazio').notEmpty()
+    req.assert('casa', 'Casa não pode ser zazio').notEmpty()
+
+    var error = req.validationErrors();
+    if (error) {
+        res.render('cadastro', { validacao: error, dadosForm: data })
+        return
+    }
+
     res.send('cadastrado');
 }
